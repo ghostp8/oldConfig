b3c460a4cbdcde302ce4651f6d65578dc955078d
ghostp8 <94147392+ghostp8@users.noreply.github.com>
2021-11-23 22:46:24 +0000

    Add files via upload

---
+++ i3/i3/lockscript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+###lock script by petvas
+# https://github.com/petvas/i3lock-blur
+# requires ffmpeg and xrandr
+# this script must be exequtible chmod +x file/path
+# bindsym $mod+z exec path/to/me
+# insure RES is correct
+
+
+TMPBG=/tmp/screen.png
+LOCK=$HOME/.config/i3/lockRick.png
+RES=1920x1080
+
+ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=5:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG
+i3lock -i $TMPBG
---
+++ i3/polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use
+# polybar -msg cmd quit
+
+# launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar polybar1 2>&1 | tee -a /tmp/polybar1.log & disown
+
+# re-cache fonts
+fc-cache -f
+
+#ech bars loaded
+echo "Bars launched..."
