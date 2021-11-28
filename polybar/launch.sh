b52098adbfc7b280fc319491958db3863615a8e2
ghostp8 <94147392+ghostp8@users.noreply.github.com>
2021-11-28 19:47:24 +0000

    Add files via upload
    removed wlan module bc network-manager-applet in tray

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,9 +5,12 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use
 # polybar -msg cmd quit
 
-R# launch bar1 and bar2
+# launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar polybar1 2>&1 | tee -a /tmp/polybar1.log & disown
-#polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 
+# re-cache fonts
+fc-cache -f
+
+#ech bars loaded
 echo "Bars launched..."
